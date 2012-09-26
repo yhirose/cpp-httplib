6818f18275417fd7ba327410a0069b885e5ee7a8
yhirose <yuji.hirose.bug@gmail.com>
2012-09-26 11:55:14 -0400

    Added SIGINT signal handler.

--- example/sample.cc
+++ example/sample.cc
@@ -7,14 +7,23 @@
 
 #include <httpsvrkit.h>
 #include <cstdio>
+#include <signal.h>
 
 using namespace httpsvrkit;
+
+template<typename Fn> void signal(int sig, Fn fn)
+{
+    static std::function<void ()> signal_handler_;
+    struct SignalHandler { static void fn(int sig) { signal_handler_(); } };
+    signal_handler_ = fn;
+    signal(sig, SignalHandler::fn);
+}
 
 int main(void)
 {
     const char* hi = "/hi";
-
     HTTP_SERVER("localhost", 1234) {
+        // svr, req, res
 
         GET("/", {
             res.set_redirect(hi);
@@ -26,6 +35,10 @@ int main(void)
 
         GET("/dump", {
             res.set_content(dump_request(cxt));
+        });
+
+        signal(SIGINT, [&](){
+            svr->stop();
         });
     }
 }
