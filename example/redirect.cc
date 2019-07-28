8480850a3f11f9f405de26cc4fef400b0102b827
yhirose <yuji.hirose.bug@gmail.com>
2019-07-28 19:09:24 -0400

    Fixed build error

--- example/redirect.cc
+++ example/redirect.cc
@@ -15,6 +15,9 @@ using namespace httplib;
 int main(void) {
   // HTTP server
   Server http;
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+  SSLServer https(SERVER_CERT_FILE, SERVER_PRIVATE_KEY_FILE);
+#endif
 
   http.Get("/test", [](const Request & /*req*/, Response &res) {
     res.set_content("Test\n", "text/plain");
@@ -25,6 +28,7 @@ int main(void) {
   });
 
   // HTTPS server
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
   SSLServer https(SERVER_CERT_FILE, SERVER_PRIVATE_KEY_FILE);
 
   https.Get("/", [=](const Request & /*req*/, Response &res) {
@@ -39,18 +43,24 @@ int main(void) {
     https.stop();
     http.stop();
   });
+#endif
 
   // Run servers
   auto httpThread = std::thread([&]() {
     http.listen("localhost", 8080);
   });
 
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
   auto httpsThread = std::thread([&]() {
     https.listen("localhost", 8081);
   });
+#endif
 
   httpThread.join();
+
+#ifdef CPPHTTPLIB_OPENSSL_SUPPORT
   httpsThread.join();
+#endif
 
   return 0;
 }
