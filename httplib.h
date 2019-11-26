d8da740597e3ab400f2939864fbed1255a048f1d
yhirose <yuji.hirose.bug@gmail.com>
2019-11-26 08:48:17 -0500

    Fix #270

--- httplib.h
+++ httplib.h
@@ -2468,7 +2468,7 @@ inline void Server::stop() {
 inline bool Server::parse_request_line(const char *s, Request &req) {
   static std::regex re(
       "(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|PRI) "
-      "(([^?]+)(?:\\?(.+?))?) (HTTP/1\\.[01])\r\n");
+      "(([^?]+)(?:\\?(.*?))?) (HTTP/1\\.[01])\r\n");
 
   std::cmatch m;
   if (std::regex_match(s, m, re)) {
