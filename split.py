e932c7e1ec526ec7dcded8336f379cc8c43bb450
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2020-01-25 17:17:34 -0600

    Update split.py
    now works with legacy Python ( < 3.0)

--- split.py
+++ split.py
@@ -2,10 +2,17 @@ import os
 
 border = '// ----------------------------------------------------------------------------'
 
+PythonVersion = sys.version_info[0];
+
 with open('httplib.h') as f:
     lines = f.readlines()
     inImplementation = False
-    os.makedirs('out', exist_ok=True)
+    
+    if PythonVersion < 3:
+        os.makedirs('out')
+    else:
+        os.makedirs('out', exist_ok=True)
+        
     with open('out/httplib.h', 'w') as fh:
         with open('out/httplib.cc', 'w') as fc:
             fc.write('#include "httplib.h"\n')
