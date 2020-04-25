7741467b37f0e0956dbec1eb975552419f057b83
Daniel Ottiger <daniel.ottiger@schindler.com>
2020-04-25 15:33:14 +0200

    disable warning C4018: '<=': signed/unsigned mismatch
    As both the type of the source and destination are a given
    it is what I intended to do: casting an size_t down to an int.
    To ensure nothing bad is happening, the code already first checks
    if the source value will fit into the destination.

--- httplib.h
+++ httplib.h
@@ -2937,6 +2937,8 @@ inline bool SocketStream::is_writable() const {
 
 inline ssize_t SocketStream::read(char *ptr, size_t size) {
 #ifdef _WIN32
+#pragma warning( push )
+#pragma warning( disable : 4018 )
   if (is_readable() && size <= std::numeric_limits<int>::max()) { return recv(sock_, ptr, static_cast<int>(size), 0); }
 #else
   if (is_readable()) { return recv(sock_, ptr, size, 0); }
@@ -2947,6 +2949,7 @@ inline ssize_t SocketStream::read(char *ptr, size_t size) {
 inline ssize_t SocketStream::write(const char *ptr, size_t size) {
 #ifdef _WIN32
   if (is_writable() && size <= std::numeric_limits<int>::max()) { return send(sock_, ptr, static_cast<int>(size), 0); }
+#pragma warning( pop )
 #else
   if (is_writable()) { return send(sock_, ptr, size, 0); }
 #endif
