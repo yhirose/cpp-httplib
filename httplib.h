53185a632e117e4b85a476a5097848a394aad5b2
Thomas Mahringer <tmahring@tmweb.at>
2020-09-10 23:00:24 +0200

    fixed forward handling with changed header parsing

--- httplib.h
+++ httplib.h
@@ -4768,7 +4768,7 @@ inline bool ClientImpl::redirect(const Request &req, Response &res) {
     return false;
   }
 
-  auto location = res.get_header_value("location");
+  auto location = detail::decode_url(res.get_header_value("location"), true);
   if (location.empty()) { return false; }
 
   const static std::regex re(
