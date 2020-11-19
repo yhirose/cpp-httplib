e1133a2dcb3436ac36c75452a569b609cdb58a0b
372046933 <372046933@users.noreply.github.com>
2020-11-19 22:21:40 +0800

    std::tolower is undefined if the argument's value is neither representable as unsigned char nor equal to EOF (#761)
    Co-authored-by: taoxu <taoxu@bilibili.com>

--- httplib.h
+++ httplib.h
@@ -271,7 +271,7 @@ struct ci {
   bool operator()(const std::string &s1, const std::string &s2) const {
     return std::lexicographical_compare(
         s1.begin(), s1.end(), s2.begin(), s2.end(),
-        [](char c1, char c2) { return ::tolower(c1) < ::tolower(c2); });
+        [](unsigned char c1, unsigned char c2) { return ::tolower(c1) < ::tolower(c2); });
   }
 };
 
