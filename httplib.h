1bcea962558b650bb789eeb696f6a69bff413c55
Yuri Santos <yrds96@protonmail.com>
2020-12-08 23:07:45 -0300

    Special function to encode query params

--- httplib.h
+++ httplib.h
@@ -1457,6 +1457,24 @@ inline bool is_valid_path(const std::string &path) {
   return true;
 }
 
+inline std::string encode_query_param(const std::string &value){
+  std::ostringstream escaped;
+  escaped.fill('0');
+  escaped << std::hex;
+
+  for (char const &c: value) {
+    if (std::isalnum(c) || c == '-' || c == '_' || c == '.'||  c == '~') {
+      escaped << c;
+    } else {
+      escaped << std::uppercase;
+      escaped << '%' << std::setw(2) << static_cast<int>(static_cast<unsigned char>(c));
+      escaped << std::nouppercase;
+    }
+  }
+
+  return escaped.str();
+}
+
 inline std::string encode_url(const std::string &s) {
   std::string result;
 
@@ -3002,7 +3020,7 @@ inline std::string params_to_query_str(const Params &params) {
     if (it != params.begin()) { query += "&"; }
     query += it->first;
     query += "=";
-    query += encode_url(it->second);
+    query += encode_query_param(it->second);
   }
   return query;
 }
