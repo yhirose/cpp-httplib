0c3fabb5a28140ed5669996b704ed239a133e1f6
Eric Lee <eric_lee@comcast.com>
2021-01-05 14:12:42 -0700

    No content check

--- httplib.h
+++ httplib.h
@@ -5488,7 +5488,7 @@ inline bool ClientImpl::process_request(Stream &strm, const Request &req,
   }
 
   // Body
-  if (req.method != "HEAD" && req.method != "CONNECT") {
+  if ((res.status != 204) && req.method != "HEAD" && req.method != "CONNECT") {
     auto out =
         req.content_receiver_
             ? static_cast<ContentReceiverWithProgress>(
