c257a0509ab5af2eec5228e1a2982a9871239f6d
David Pfahler <david@pfahler.at>
2021-09-28 19:06:59 +0200

    Update split.py

--- split.py
+++ split.py
@@ -30,35 +30,38 @@ cc_out = args.out + source_name
 
 # if the modification time of the out file is after the in file,
 # don't split (as it is already finished)
+do_split = True
+
 if os.path.exists(h_out):
     in_time = os.path.getmtime(in_file)
     out_time = os.path.getmtime(h_out)
-    if in_time < out_time:
-        print("{} and {} are up to date".format(h_out, cc_out))
-        exit
-        
-with open(in_file) as f:
-    lines = f.readlines()
+    do_split = in_time > out_time:
 
-python_version = sys.version_info[0]
-if python_version < 3:
-    os.makedirs(args.out)
+if do_split:
+    with open(in_file) as f:
+        lines = f.readlines()
+
+    python_version = sys.version_info[0]
+    if python_version < 3:
+        os.makedirs(args.out)
+    else:
+        os.makedirs(args.out, exist_ok=True)
+
+    in_implementation = False
+    cc_out = args.out + source_name
+    with open(h_out, 'w') as fh, open(cc_out, 'w') as fc:
+        fc.write('#include "httplib.h"\n')
+        fc.write('namespace httplib {\n')
+        for line in lines:
+            is_border_line = border in line
+            if is_border_line:
+                in_implementation = not in_implementation
+            elif in_implementation:
+                fc.write(line.replace('inline ', ''))
+            else:
+                fh.write(line)
+        fc.write('} // namespace httplib\n')
+
+    print("Wrote {} and {}".format(h_out, cc_out))
 else:
-    os.makedirs(args.out, exist_ok=True)
-
-in_implementation = False
-cc_out = args.out + source_name
-with open(h_out, 'w') as fh, open(cc_out, 'w') as fc:
-    fc.write('#include "httplib.h"\n')
-    fc.write('namespace httplib {\n')
-    for line in lines:
-        is_border_line = border in line
-        if is_border_line:
-            in_implementation = not in_implementation
-        elif in_implementation:
-            fc.write(line.replace('inline ', ''))
-        else:
-            fh.write(line)
-    fc.write('} // namespace httplib\n')
-
-print("Wrote {} and {}".format(h_out, cc_out))
+    print("{} and {} are up to date".format(h_out, cc_out))
