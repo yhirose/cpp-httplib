ca3a0156a3db2d4eaf09738c71233b120bfcf870
vitaly-ivanov <37587207+witaly-iwanow@users.noreply.github.com>
2021-12-30 09:15:46 +0700

    changes wrapped into ifdef(_WIN32) just in case

--- httplib.h
+++ httplib.h
@@ -214,10 +214,12 @@ using socket_t = int;
 
 #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
 // these are defined in wincrypt.h and it breaks compilation if BoringSSL is used
+#if defined(_WIN32)
 #undef X509_NAME
 #undef X509_CERT_PAIR
 #undef X509_EXTENSIONS
 #undef PKCS7_SIGNER_INFO
+#endif
 
 #include <openssl/err.h>
 #include <openssl/md5.h>
