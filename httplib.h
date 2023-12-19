b1def992b4828ba6581a226550291ce9d28f3350
bugdea1er <bugdealer@icloud.com>
2023-12-20 01:12:51 +0300

    Remove docs for StatusCode constants

--- httplib.h
+++ httplib.h
@@ -1793,77 +1793,77 @@ inline void default_socket_options(socket_t sock) {
 
 enum StatusCode {
   // Information responses
-  Continue_100 = 100,                      ///< 100 Continue
-  SwitchingProtocol_101 = 101,             ///< 101 Switching Protocols
-  Processing_102 = 102,                    ///< 102 Processing
-  EarlyHints_103 = 103,                    ///< 103 Early Hints
+  Continue_100 = 100,
+  SwitchingProtocol_101 = 101,
+  Processing_102 = 102,
+  EarlyHints_103 = 103,
 
   // Successful responses
-  OK_200 = 200,                            ///< 200 OK
-  Created_201 = 201,                       ///< 201 Created
-  Accepted_202 = 202,                      ///< 202 Accepted
-  NonAuthoritativeInformation_203 = 203,   ///< 203 Non-Authoritative Information
-  NoContent_204 = 204,                     ///< 204 No Content
-  ResetContent_205 = 205,                  ///< 205 Reset Content
-  PartialContent_206 = 206,                ///< 206 Partial Content
-  MultiStatus_207 = 207,                   ///< 207 Multi-Status
-  AlreadyReported_208 = 208,               ///< 208 Already Reported
-  IMUsed_226 = 226,                        ///< 226 IM Used
+  OK_200 = 200,
+  Created_201 = 201,
+  Accepted_202 = 202,
+  NonAuthoritativeInformation_203 = 203,
+  NoContent_204 = 204,
+  ResetContent_205 = 205,
+  PartialContent_206 = 206,
+  MultiStatus_207 = 207,
+  AlreadyReported_208 = 208,
+  IMUsed_226 = 226,
 
   // Redirection messages
-  MultipleChoices_300 = 300,               ///< 300 Multiple Choices
-  MovedPermanently_301 = 301,              ///< 301 Moved Permanently
-  Found_302 = 302,                         ///< 302 Found
-  SeeOther_303 = 303,                      ///< 303 See Other
-  NotModified_304 = 304,                   ///< 304 Not Modified
-  UseProxy_305 = 305,                      ///< 305 Use Proxy
-  unused_306 = 306,                        ///< 306 unused
-  TemporaryRedirect_307 = 307,             ///< 307 Temporary Redirect
-  PermanentRedirect_308 = 308,             ///< 308 Permanent Redirect
+  MultipleChoices_300 = 300,
+  MovedPermanently_301 = 301,
+  Found_302 = 302,
+  SeeOther_303 = 303,
+  NotModified_304 = 304,
+  UseProxy_305 = 305,
+  unused_306 = 306,
+  TemporaryRedirect_307 = 307,
+  PermanentRedirect_308 = 308,
 
   // Client error responses
-  BadRequest_400 = 400,                    ///< 400 Bad Request
-  Unauthorized_401 = 401,                  ///< 401 Unauthorized
-  PaymentRequired_402 = 402,               ///< 402 Payment Required
-  Forbidden_403 = 403,                     ///< 403 Forbidden
-  NotFound_404 = 404,                      ///< 404 Not Found
-  MethodNotAllowed_405 = 405,              ///< 405 Method Not Allowed
-  NotAcceptable_406 = 406,                 ///< 406 Not Acceptable
-  ProxyAuthenticationRequired_407 = 407,   ///< 407 Proxy Authentication Required
-  RequestTimeout_408 = 408,                ///< 408 Request Timeout
-  Conflict_409 = 409,                      ///< 409 Conflict
-  Gone_410 = 410,                          ///< 410 Gone
-  LengthRequired_411 = 411,                ///< 411 Length Required
-  PreconditionFailed_412 = 412,            ///< 412 Precondition Failed
-  PayloadTooLarge_413 = 413,               ///< 413 Payload Too Large
-  UriTooLong_414 = 414,                    ///< 414 URI Too Long
-  UnsupportedMediaType_415 = 415,          ///< 415 Unsupported Media Type
-  RangeNotSatisfiable_416 = 416,           ///< 416 Range Not Satisfiable
-  ExpectationFailed_417 = 417,             ///< 417 Expectation Failed
-  ImATeapot_418 = 418,                     ///< 418 I'm a teapot
-  MisdirectedRequest_421 = 421,            ///< 421 Misdirected Request
-  UnprocessableContent_422 = 422,          ///< 422 Unprocessable Content
-  Locked_423 = 423,                        ///< 423 Locked
-  FailedDependency_424 = 424,              ///< 424 Failed Dependency
-  TooEarly_425 = 425,                      ///< 425 Too Early
-  UpgradeRequired_426 = 426,               ///< 426 Upgrade Required
-  PreconditionRequired_428 = 428,          ///< 428 Precondition Required
-  TooManyRequests_429 = 429,               ///< 429 Too Many Requests
-  RequestHeaderFieldsTooLarge_431 = 431,   ///< 431 Request Header Fields Too Large
-  UnavailableForLegalReasons_451 = 451,    ///< 451 Unavailable For Legal Reasons
+  BadRequest_400 = 400,
+  Unauthorized_401 = 401,
+  PaymentRequired_402 = 402,
+  Forbidden_403 = 403,
+  NotFound_404 = 404,
+  MethodNotAllowed_405 = 405,
+  NotAcceptable_406 = 406,
+  ProxyAuthenticationRequired_407 = 407,
+  RequestTimeout_408 = 408,
+  Conflict_409 = 409,
+  Gone_410 = 410,
+  LengthRequired_411 = 411,
+  PreconditionFailed_412 = 412,
+  PayloadTooLarge_413 = 413,
+  UriTooLong_414 = 414,
+  UnsupportedMediaType_415 = 415,
+  RangeNotSatisfiable_416 = 416,
+  ExpectationFailed_417 = 417,
+  ImATeapot_418 = 418,
+  MisdirectedRequest_421 = 421,
+  UnprocessableContent_422 = 422,
+  Locked_423 = 423,
+  FailedDependency_424 = 424,
+  TooEarly_425 = 425,
+  UpgradeRequired_426 = 426,
+  PreconditionRequired_428 = 428,
+  TooManyRequests_429 = 429,
+  RequestHeaderFieldsTooLarge_431 = 431,
+  UnavailableForLegalReasons_451 = 451,
 
   // Server error responses
-  InternalServerError_500 = 500,           ///< 500 Internal Server Error
-  NotImplemented_501 = 501,                ///< 501 Not Implemented
-  BadGateway_502 = 502,                    ///< 502 Bad Gateway
-  ServiceUnavailable_503 = 503,            ///< 503 Service Unavailable
-  GatewayTimeout_504 = 504,                ///< 504 Gateway Timeout
-  HttpVersionNotSupported_505 = 505,       ///< 505 HTTP Version Not Supported
-  VariantAlsoNegotiates_506 = 506,         ///< 506 Variant Also Negotiates
-  InsufficientStorage_507 = 507,           ///< 507 Insufficient Storage
-  LoopDetected_508 = 508,                  ///< 508 Loop Detected
-  NotExtended_510 = 510,                   ///< 510 Not Extended
-  NetworkAuthenticationRequired_511 = 511, ///< 511 Network Authentication Required
+  InternalServerError_500 = 500,
+  NotImplemented_501 = 501,
+  BadGateway_502 = 502,
+  ServiceUnavailable_503 = 503,
+  GatewayTimeout_504 = 504,
+  HttpVersionNotSupported_505 = 505,
+  VariantAlsoNegotiates_506 = 506,
+  InsufficientStorage_507 = 507,
+  LoopDetected_508 = 508,
+  NotExtended_510 = 510,
+  NetworkAuthenticationRequired_511 = 511,
 };
 
 inline const char *status_message(int status) {
