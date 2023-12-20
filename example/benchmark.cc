17976c2f6f70a335a1a1778ea4c3c3ae5af24ee8
bugdea1er <bugdealer@icloud.com>
2023-12-20 21:11:20 +0300

    Use StatusCode in tests and examples

--- example/benchmark.cc
+++ example/benchmark.cc
@@ -26,7 +26,7 @@ int main(void) {
   for (int i = 0; i < 3; i++) {
     StopWatch sw(to_string(i).c_str());
     auto res = cli.Post("/post", body, "application/octet-stream");
-    assert(res->status == 200);
+    assert(res->status == httplib::StatusCode::OK_200);
   }
 
   return 0;
