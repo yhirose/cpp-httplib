72601762b9674136447483d37edc4efbe138ab9e
Arnaud Bienner <abien@dolby.com>
2024-02-29 16:08:40 +0100

    Performance: reserve body to avoid frequent reallocations and copies

--- httplib.h
+++ httplib.h
@@ -7461,6 +7461,21 @@ inline bool ClientImpl::process_request(Stream &strm, Request &req,
     error = Error::Read;
     return false;
   }
+  
+  // If we know the content length, reserve the expected size in the output
+  // buffer to avoid re-allocations and copies when appending received data
+  auto it = res.headers.find("Content-Length");
+  if (it != res.headers.end()) {
+    try {
+      int content_length = std::stoi(it->second);
+      // Check for unexpected values
+      if (content_length > 0 && content_length < 256*1024*1024 /* 256 MB */) {
+        res.body.reserve(content_length);
+      }
+    } catch(...) {
+      // Conversion error: ignore it. We will act like if no Content-Length header was provided
+    }
+  }
 
   // Body
   if ((res.status != StatusCode::NoContent_204) && req.method != "HEAD" &&
