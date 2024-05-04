50dde7d676f7172ae76a9e90becfcac2fc479ae2
Sean Quinn <hubbleslaw@gmail.com>
2024-05-04 11:14:06 -0700

    Add UT for ipv6 + Universal client implementation

--- httplib.h
+++ httplib.h
@@ -9250,6 +9250,8 @@ inline Client::Client(const std::string &scheme_host_port,
                                              client_key_path);
     }
   } else {
+    // NOTE: Update TEST(UniversalClientImplTest, Ipv6LiteralAddress)
+    // if port param below changes.
     cli_ = detail::make_unique<ClientImpl>(scheme_host_port, 80,
                                            client_cert_path, client_key_path);
   }
