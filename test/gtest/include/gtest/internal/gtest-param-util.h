d7dafbd6b40638f31a92eeb329abe8386e519b48
bigmoonbit <bigmoonbit@outlook.com>
2025-11-27 14:54:55 +0800

    chore: fix some typos in comments
    Signed-off-by: bigmoonbit <bigmoonbit@outlook.com>

--- test/gtest/include/gtest/internal/gtest-param-util.h
+++ test/gtest/include/gtest/internal/gtest-param-util.h
@@ -753,7 +753,7 @@ class ParameterizedTestSuiteRegistry {
 };
 
 // Keep track of what type-parameterized test suite are defined and
-// where as well as which are intatiated. This allows susequently
+// where as well as which are intatiated. This allows subsequently
 // identifying suits that are defined but never used.
 class TypeParameterizedTestSuiteRegistry {
  public:
@@ -764,7 +764,7 @@ class TypeParameterizedTestSuiteRegistry {
   // Add an instantiation of a suit.
   void RegisterInstantiation(const char* test_suite_name);
 
-  // For each suit repored as defined but not reported as instantiation,
+  // For each suit reported as defined but not reported as instantiation,
   // emit a test that reports that fact (configurably, as an error).
   void CheckForInstantiations();
 
