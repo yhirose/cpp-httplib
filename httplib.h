69ff0fe704818dfcfb015530342a760cc6999420
yhirose <yuji.hirose.bug@gmail.com>
2026-01-16 18:18:37 -0500

    clang-format

--- httplib.h
+++ httplib.h
@@ -4788,9 +4788,7 @@ inline int getaddrinfo_with_timeout(const char *node, const char *service,
 
   // Allocate on the heap, so the resolver thread can keep using the data.
   auto state = std::make_shared<GetAddrInfoState>();
-  if (node) {
-    state->node = node;
-  }
+  if (node) { state->node = node; }
   state->service = service;
   state->hints = *hints;
 
